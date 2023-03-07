5941ad2408a8304dd7a17a866eeed2602e021c78
tj4752 <tanujsinghkushwah@gmail.com>
2023-03-08 01:54:45 +0530

    oAuth service hygiene fixes

--- app/src/main/java/com/example/ethervpn/MainActivity.java
+++ app/src/main/java/com/example/ethervpn/MainActivity.java
@@ -1,121 +1,33 @@
 package com.example.ethervpn;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
-    SignInButton btSignIn;
-    GoogleSignInClient googleSignInClient;
-    FirebaseAuth firebaseAuth;
+    SharedPreferences sharedpreferences;
+
+    public static final String mypreference = "appPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Assign variable
-        btSignIn = findViewById(R.id.sign_in_button);
+        sharedpreferences = getSharedPreferences(mypreference, MODE_PRIVATE);
 
-        // Initialize sign in options the client-id is copied form google-services.json file
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("1094407803885-gdih713nm1hkl31a2vbg8bgbc26srhu4.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
+        boolean isLoggedIn = sharedpreferences.getBoolean("isLoggedIn", false);
 
-        // Initialize sign in client
-        googleSignInClient = GoogleSignIn.getClient(MainActivity.this, googleSignInOptions);
-
-        btSignIn.setOnClickListener((View.OnClickListener) view -> {
-            // Initialize sign in intent
-            Intent intent = googleSignInClient.getSignInIntent();
-            // Start activity for result
-            startActivityForResult(intent, 100);
-
-        });
-
-        // Initialize firebase auth
-        firebaseAuth = FirebaseAuth.getInstance();
-        // Initialize firebase user
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        // Check condition
-        if (firebaseUser != null) {
-            // When user already sign in redirect to profile activity
-            startActivity(new Intent(MainActivity.this, ProfileActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        Intent intent;
+        if (isLoggedIn) {
+            intent = new Intent(this, ProfileActivity.class);
+        } else {
+            intent = new Intent(this, OAuthService.class);
         }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Check condition
-        if (requestCode == 100) {
-            // When request code is equal to 100 initialize task
-            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-            // check condition
-            if (signInAccountTask.isSuccessful()) {
-                // When google sign in successful initialize string
-                String s = "Google sign in successful";
-                // Display Toast
-                displayToast(s);
-                // Initialize sign in account
-                try {
-                    // Initialize sign in account
-                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
-                    // Check condition
-                    if (googleSignInAccount != null) {
-                        // When sign in account is not equal to null initialize auth credential
-                        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
-                        // Check credential
-                        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                // Check condition
-                                if (task.isSuccessful()) {
-                                    // When task is successful redirect to profile activity display Toast
-                                    startActivity(new Intent(MainActivity.this, ProfileActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                                    displayToast("Firebase authentication successful");
-                                } else {
-                                    // When task is unsuccessful display Toast
-                                    displayToast("Authentication Failed :" + task.getException().getMessage());
-                                }
-                            }
-                        });
-                    }
-                } catch (ApiException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private void displayToast(String s) {
-        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/com/example/ethervpn/ProfileActivity.java
+++ app/src/main/java/com/example/ethervpn/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.example.ethervpn;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,52 +19,56 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
-    // Initialize variable
+
     ImageView ivImage;
+
     TextView tvName;
+
     Button btLogout;
+
     FirebaseAuth firebaseAuth;
+
     GoogleSignInClient googleSignInClient;
+
+    SharedPreferences sharedPreferences;
+
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        // Assign variable
+        sharedPreferences = getSharedPreferences("appPreferences",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
         ivImage = findViewById(R.id.iv_image);
         tvName = findViewById(R.id.tv_name);
         btLogout = findViewById(R.id.bt_logout);
 
-        // Initialize firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
 
-        // Initialize firebase user
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-        // Check condition
         if (firebaseUser != null) {
-            // When firebase user is not equal to null set image on image view
             Glide.with(ProfileActivity.this).load(firebaseUser.getPhotoUrl()).into(ivImage);
-            // set name on text view
             tvName.setText(firebaseUser.getDisplayName());
         }
 
-        // Initialize sign in client
         googleSignInClient = GoogleSignIn.getClient(ProfileActivity.this, GoogleSignInOptions.DEFAULT_SIGN_IN);
 
         btLogout.setOnClickListener(view -> {
-            // Sign out from google
             googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                    // Check condition
                     if (task.isSuccessful()) {
-                        // When task is successful sign out from firebase
                         firebaseAuth.signOut();
-                        // Display Toast
                         Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_SHORT).show();
-                        // Finish activity
+
+                        editor.putBoolean("isLoggedIn", false);
+                        editor.apply();
+
+                        startActivity(new Intent(ProfileActivity.this, OAuthService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                         finish();
                     }
                 }
